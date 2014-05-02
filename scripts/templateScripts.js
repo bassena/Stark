f638b6695eba44a74a63c34e6152bd51c5b1ea38
bassena <mr.sheepness@gmail.com>
2014-05-02 18:47:29 -0400

    Still updating template drop-down menu

--- scripts/templateScripts.js
+++ scripts/templateScripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-	$('#gameSelector').accordion();
+	$('#gameSelector').accordion({
+		collapsible:true,
+		active: false,
+		animated:'bounceslide',
+		heightStyle: 'content'
+	});
+	$('#gameSelector > h3').addClass('ui-corner-bottom');
+	$('#games').menu();
 });
