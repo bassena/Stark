f52faff5c0fc0e831e9bbb848f0f16813a0a0701
bassena <mr.sheepness@gmail.com>
2014-05-09 18:41:56 -0400

    Select game page now complete
    Please no more editing unless needed.

--- scripts/selectGameScripts.js
+++ scripts/selectGameScripts.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
 	$('#roulette').hover(function(){
-		$('#roulette').animate({
-			backgroundColor: 'rgba(200,0,0,0.5)'
-		});
+		$(this).animate({
+			backgroundColor: 'rgba(255,255,255,0.5)'
+		}, 200);
 	}, function(){
-		
+		$(this).animate({
+			backgroundColor: 'rgba(0,0,0,0.5)'
+		},200);
+	});
+	
+	$('#blkjk').hover(function(){
+		$(this).animate({
+			backgroundColor: 'rgba(255,255,255,0.5)'
+		},200);
+	}, function(){
+		$(this).animate({
+			backgroundColor: 'rgba(0,0,0,0.5)'
+		},200);
+	});
+	
+	$('#poker').hover(function(){
+		$(this).animate({
+			backgroundColor: 'rgba(255,255,255,0.5)'
+		},200);
+	}, function(){
+		$(this).animate({
+			backgroundColor: 'rgba(0,0,0,0.5)'
+		},200);
+	});
+	
+	$('#slots').hover(function(){
+		$(this).animate({
+			backgroundColor: 'rgba(255,255,255,0.5)'
+		},200);
+	}, function(){
+		$(this).animate({
+			backgroundColor: 'rgba(0,0,0,0.5)'
+		},200);
 	});
 });
